831d8d4c185b2e4e0c26c7e2350a1570ce11e576
Norman Azares <azares.norman.ccs@gmail.com>
2022-05-07 21:04:37 +0800

    commit

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ const navLinks = document.querySelector(".nav-bar").querySelectorAll('a')
     }
 })
 
+// PhoneBrands Active Onclick Function
+var elements = document.querySelector(".phonebrands").querySelectorAll('li')
+
+var myFunction = function(e) {
+    if (document.querySelector('.active-brand') !== null) {
+      document.querySelector('.active-brand').classList.remove('active-brand');
+    }
+    e.target.className = "active-brand";
+}
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', myFunction, false);
+}
