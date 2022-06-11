db9462aca6faf36b2cd9f80d9a6b2f420414add7
Norman Azares <azares.norman.ccs@gmail.com>
2022-06-12 00:59:11 +0800

    commit

--- promo.js
+++ promo.js
@@ -12,3 +12,12 @@
 // }
 
 // window.onload = slideShow;
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 2){
+        counter = 1;
+    }
+}, 5000);
